790956f36c433e758c9ce4af090de99de86e5120
Michael Joyce <ubermichael@gmail.com>
2020-08-28 10:20:02 -0700

    cleaning up some old files.

--- migrations/2020/01/Version20200103220825.php
+++ migrations/2020/01/Version20200103220825.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-namespace App\Migrations;
+namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
@@ -16,12 +16,12 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200103220200 extends AbstractMigration {
+final class Version20200103220825 extends AbstractMigration {
     public function up(Schema $schema) : void {
-        $this->addSql('CREATE FULLTEXT INDEX IDX_AF3C67792B36786B75355FA1 ON publication (title, abstract)');
+        $this->addSql('CREATE FULLTEXT INDEX IDX_3A9F98E55E237E06F47645AE ON institution (name, url)');
     }
 
     public function down(Schema $schema) : void {
-        $this->addSql('DROP INDEX IDX_AF3C67792B36786B75355FA1 ON publication');
+        $this->addSql('DROP INDEX IDX_3A9F98E55E237E06F47645AE ON institution');
     }
 }
